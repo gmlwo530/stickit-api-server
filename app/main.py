1f179179a7d6f0b28139f68644f13a90068d5405
CHOI HEE JAE <gmlwo530@gmail.com>
2021-05-09 00:50:53 +0900

    Update on event

--- app/main.py
+++ app/main.py
@@ -3,7 +3,19 @@ from fastapi import FastAPI
 
 from app.api.api_v1.api import api_router
 from app.core import config
+from app.db.database import get_database, close_connection
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def startup():
+    get_database()
+
+
+@app.on_event("shutdown")
+def shutdown():
+    close_connection()
+
 
 app.include_router(api_router, prefix=config.API_V1_STR)
