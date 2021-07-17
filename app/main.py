bdda8de6b01cc6f388ae43950c8ef5da9971832c
CHOI HEE JAE <gmlwo530@gmail.com>
2021-07-17 15:46:02 +0900

    Edit: docker

--- app/main.py
+++ app/main.py
@@ -1,26 +1,26 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 
-
 from app.api.api_v1.api import api_router
 from app.core import config
-from app.db.database import get_database, close_connection
 
-app = FastAPI(
-    title="Pixhelves API Docs",
-    description="Pixhelves API",
-    version="0.0.1"
-)
+
+# from db.database import get_database, close_connection
+
+
+app = FastAPI(title="Pixhelves API Docs", description="Pixhelves API", version="0.0.1")
 
 
 @app.on_event("startup")
 def startup():
-    get_database()
+    # get_database()
+    pass
 
 
 @app.on_event("shutdown")
 def shutdown():
-    close_connection()
+    # close_connection()
+    pass
 
 
 app.include_router(api_router, prefix=config.API_V1_STR)
